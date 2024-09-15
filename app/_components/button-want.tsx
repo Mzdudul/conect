adaa0fa497bf3589ff648c0ed14a472ac24268c9
Mzdudul <duduzim.gon05@gmail.com>
2024-09-14 22:07:50 -0300

    deploy final

--- app/_components/button-want.tsx
+++ app/_components/button-want.tsx
@@ -5,7 +5,7 @@ const ButtonWant = () => {
     <div>
       <Button
         variant="outline"
-        className="bg-yellow-400 border-none  transition ease-in-out delay-150  hover:-translate-y-1 hover:scale-110  duration-300 text-xl md:lg:text-2xl flex items-center font-medium w-72  md:lg:w-full hover:bg-yellow-400 h-14 mt-5 rounded-xl"
+        className="bg-red-600 border-none  transition ease-in-out delay-150  hover:-translate-y-1 hover:scale-110  duration-300 text-xl md:lg:text-2xl flex items-center font-medium w-72  md:lg:w-full hover:bg-yellow-400 h-14 mt-5 rounded-xl"
       >
         QUERO ESSE!
       </Button>
